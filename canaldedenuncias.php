345f6258ec8b0f9fc7facfd59bd92b55bd87385e
pcShouxin <stipro150197@gmail.com>
2022-12-05 15:39:56 -0500

    Menu Canal de denuncias

---
+++ canaldedenuncias.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Canal de Denuncias | Minera Shouxin Peru S.A.</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Raleway:300,400,500,700,800" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <div class="container" id="container-canal-noticias">
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4" g-5">
+            <div class="col card-canal-noticias">
+                <div class="card container-card-canal-noticias">
+                    <img src="./assets/img/canal-de-denuncias/actos-corrupcion.webp" class="card-img-top img-canal-denuncias" alt="Hollywood Sign on The Hill" />
+                    <div class="card-body">
+                        <h5 class="card-title text-canal-denuncias">Acto de corrupción/ <br>Otros</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col card-canal-noticias">
+                <div class="card container-card-canal-noticias">
+                    <img src="./assets/img/canal-de-denuncias/denuncias-hostigamiento-laboral.webp" class="card-img-top img-canal-denuncias" alt="Palm Springs Road" />
+                    <div class="card-body">
+                        <h5 class="card-title text-canal-denuncias">Denuncias sobre <br>Hostigamiento Laboral</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col card-canal-noticias">
+                <div class="card container-card-canal-noticias">
+                    <img src="./assets/img/canal-de-denuncias/denuncia-reclamo-laboral.webp" class="card-img-top img-canal-denuncias" alt="Los Angeles Skyscrapers" />
+                    <div class="card-body">
+                        <h5 class="card-title text-canal-denuncias">Denuncia/Reclamo Laboral</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col card-canal-noticias">
+                <div class="card container-card-canal-noticias">
+                    <img src="./assets/img/canal-de-denuncias/relaciones-comunitarios.webp" class="card-img-top img-canal-denuncias" alt="Skyscrapers" />
+                    <div class="card-body">
+                        <h5 class="card-title text-canal-denuncias">Consulta/Denuncia/<br>Reclamos al cumplimiento</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col card-canal-noticias">
+                <div class="card container-card-canal-noticias">
+                    <img src="./assets/img/canal-de-denuncias/sistema-anticorrupcion.jpeg" class="card-img-top img-canal-denuncias" alt="Skyscrapers" />
+                    <div class="card-body">
+                        <h5 class="card-title text-canal-denuncias">Prevención y Control del <br>Sistema Anticorrupción</h5>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</body>
+
+</html>

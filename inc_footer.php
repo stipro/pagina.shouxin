50375fb063f3e04663ecd3f0c17a526e2386a0de
pcShouxin <stipro150197@gmail.com>
2023-01-02 08:06:46 -0500

    Arrgelando footer

---
+++ inc_footer.php
@@ -0,0 +1,64 @@
+<footer id="footer">
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-3 col-md-6 footer-info">
+                    <img src="assets/img/Logo-minera-Frank-2-r.png" alt="TheEvenet">
+                </div>
+
+                <div class="col-lg-3 col-md-6 footer-links">
+                    <h4>Enlaces</h4>
+                    <ul>
+                        <li><i class="bi bi-chevron-right"></i> <a href="#">Inicio</a></li>
+                        <li><i class="bi bi-chevron-right"></i> <a href="#about">Sobre nosotros</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-3 col-md-6 footer-links">
+                    <!-- <h4>Useful Links</h4>
+            <ul>
+              <li><i class="bi bi-chevron-right"></i> <a href="#">Home</a></li>
+              <li><i class="bi bi-chevron-right"></i> <a href="#">About us</a></li>
+              <li><i class="bi bi-chevron-right"></i> <a href="#">Services</a></li>
+              <li><i class="bi bi-chevron-right"></i> <a href="#">Terms of service</a></li>
+              <li><i class="bi bi-chevron-right"></i> <a href="#">Privacy policy</a></li>
+            </ul> -->
+                </div>
+
+                <div class="col-lg-3 col-md-6 footer-contact">
+                    <h4>Contacta con nosotros</h4>
+                    <p>
+                        Marcona <br>
+                        Ica<br>
+                        Perú <br>
+                        <strong>Email:</strong> -<br>
+                    </p>
+
+                    <div class="social-links">
+                        <a href="https://es-la.facebook.com/ShouxinPeru/" class="facebook"><i class="bi bi-facebook"></i></a>
+                        <!-- <a href="#" class="google-plus"><i class="bi bi-instagram"></i></a> -->
+                        <a href="https://pe.linkedin.com/company/minera-shouxin-per%C3%BA" class="linkedin"><i class="bi bi-linkedin"></i></a>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="copyright">
+            &copy; Copyright <strong>Minera Shouxin Peru S.A.</strong>. Reservados todos los derechos
+        </div>
+        <div class="credits">
+            <!--
+        All the links in the footer should remain intact.
+        You can delete the links only if you purchased the pro version.
+        Licensing information: https://bootstrapmade.com/license/
+        Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=TheEvent
+      -->
+            Designed by <a href="#">-</a>
+        </div>
+    </div>
+</footer>
--- reclamoLaboral.php
+++ reclamoLaboral.php
@@ -158,38 +158,8 @@
     </main><!-- End #main -->
 
     <!-- ======= Footer ======= -->
-    <footer id="footer">
-        <div class="footer-top">
-            <div class="container">
-                <div class="row">
-
-                    <div class="col-lg-3 col-md-6 footer-info">
-                        <img src="assets/img/logo.png" alt="TheEvenet">
-                        <!-- <p>In alias aperiam. Placeat tempore facere. Officiis voluptate ipsam vel eveniet est dolor et
-                            totam porro. Perspiciatis ad omnis fugit molestiae recusandae possimus. Aut consectetur id
-                            quis. In inventore consequatur ad voluptate cupiditate debitis accusamus repellat cumque.
-                        </p> -->
-                    </div>
-
-                </div>
-            </div>
-        </div>
-
-        <div class="container">
-            <div class="copyright">
-                &copy; Copyright <strong>Shouxin Peru S.A.</strong>. Todos los derechos reservados
-            </div>
-            <div class="credits">
-                <!--
-        All the links in the footer should remain intact.
-        You can delete the links only if you purchased the pro version.
-        Licensing information: https://bootstrapmade.com/license/
-        Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=TheEvent
-      -->
-                <!-- Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a> -->
-            </div>
-        </div>
-    </footer><!-- End  Footer -->
+    <?php require_once 'inc_footer.php'; ?>
+    <!-- End  Footer -->
 
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
